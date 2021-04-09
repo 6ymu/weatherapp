f0d18561013fc56d2a8c6b6f7d4ddf2120a49a94
Mukhan Zamanbekov <mukh4n@gmail.com>
2021-04-09 11:08:29 +0600

    Добавьте файлы проекта.

---
+++ WpfApp1/applicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WpfApp1
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<WeatherList> historyWeather { get; set; }
+        public ApplicationContext() : base("database.db")
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/weatherlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class WeatherList
+    {
+        public int id { set; get; }
+        public string date { set; get; }
+        public string city { set; get; }
+        public int temp  { set; get; }
+        public int temp_feels { set; get; }
+        public int humidity { set; get; }
+        public int wind { set; get; }
+        public int temp_min { set; get; }
+        public int temp_max { set; get; }
+    }
+}
